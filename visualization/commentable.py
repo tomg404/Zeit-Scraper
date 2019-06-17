ff3beb2da41fbf2db513404f5c15d31121ef7251
tomg404 <32769321+tomg404@users.noreply.github.com>
2019-06-17 16:00:15 +0200

    Small change

--- visualization/commentable.py
+++ visualization/commentable.py
@@ -3,17 +3,27 @@ Example script to generate a bar chart which shows the number of commentable
 articles on Zeit.de
 '''
 
+import sys
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
 
-data = 'data.csv'
+# path where the plot gets saved
+SAVE_PATH = Path().absolute().joinpath('visualization/').joinpath('plots/').joinpath('commentable.png')
+
+# pass csv file via commandline
+data = sys.argv[1]
 d = {}
 
+# row where the ressorts are in the csv file
 rowNumber = 6
 
-with open(data, 'r', encoding='utf-8') as csvFile:
+with open(data, 'r', encoding='utf-8', newline='') as csvFile:
 	reader = csv.reader(csvFile, delimiter=',')
-	next(reader)	# skip the first line IMPORTANT
+	next(reader)	# skip the first two lines IMPORTANT
+	next(reader)
+
 	for row in reader:
 		if row[rowNumber] not in d:
 			d[str(row[rowNumber])] = 1
@@ -22,18 +32,21 @@ with open(data, 'r', encoding='utf-8') as csvFile:
 
 print(d)
 
-import matplotlib.pyplot as plt
+
+
 objects = tuple(key for key in d.keys())
 y_pos = np.arange(len(objects))
 number = d.values()
 
-barlist = plt.bar(y_pos, number, align='center', color='c')
+plt.style.use('ggplot')
+barlist = plt.bar(y_pos, number, align='center')
 barlist[0].set_color('#60BD68')
 barlist[1].set_color('#F15854')
-barlist[2].set_color('#4D4D4D')
+
 plt.xticks(y_pos, objects)
 plt.xlabel('Commentable?')
 plt.ylabel('Number')
 plt.title('Number of commentable articles on Zeit.de')
 
+plt.savefig(SAVE_PATH)
 plt.show()
