f4a909c5ffb42b5c69a18e703b1b6b131615d7cd
tomg404 <32769321+tomg404@users.noreply.github.com>
2019-06-17 15:59:21 +0200

    small fix

--- src/writer.py
+++ src/writer.py
@@ -3,11 +3,11 @@ from src.vars import CSV_FILE, FIELDNAMES, DELIMITER
 
 # creates new csv file
 def create_new(name):
-    with open(name, 'w') as csvfile:
+    with open(name, 'w', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile, delimiter=DELIMITER)
         writer.writerow(FIELDNAMES)
 
 def insert_data(dict):
-    with open(str(CSV_FILE), mode='a', newline='') as csvfile:
+    with open(str(CSV_FILE), mode='a', newline='', encoding='utf-8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
         writer.writerow(dict)
